8e35e3617ab7d28702e29394804776ee2e54f2ce
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-02-26 11:55:54 +0100

    Add tests for previous element

--- tdd-exercise/src/test/java/CircularListTest.java
+++ tdd-exercise/src/test/java/CircularListTest.java
@@ -59,4 +59,27 @@ public class CircularListTest {
         assertEquals(circularList.next().orElse(null), element1);
     }
 
+    @Test
+    public void testPreviousElement() {
+        final int element = 1;
+        circularList.add(element);
+        circularList.add(element + 1);
+        circularList.next();
+        assertEquals(circularList.previous().orElse(null), element);
+    }
+
+    @Test
+    public void testPreviousElementCircular() {
+        final int element1 = 1;
+        final int element2 = 2;
+        final int element3 = 3;
+        circularList.add(element1);
+        circularList.add(element2);
+        circularList.add(element3);
+        assertEquals(circularList.previous().orElse(null), element3);
+        assertEquals(circularList.previous().orElse(null), element2);
+        assertEquals(circularList.previous().orElse(null), element1);
+        assertEquals(circularList.previous().orElse(null), element3);
+    }
+
 }
