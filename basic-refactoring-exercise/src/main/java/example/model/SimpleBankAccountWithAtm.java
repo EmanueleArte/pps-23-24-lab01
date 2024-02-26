c8085b3e4e8a72e4a65da11e5c49abc45ecfeaac
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-02-26 11:11:38 +0100

    Extended SimpleBankAccount methods

--- basic-refactoring-exercise/src/main/java/example/model/SimpleBankAccountWithAtm.java
+++ basic-refactoring-exercise/src/main/java/example/model/SimpleBankAccountWithAtm.java
@@ -6,7 +6,32 @@ package example.model;
  * while the withdrawal is allowed only if the balance greater or equal the withdrawal amount plus the fee
  */
 public class SimpleBankAccountWithAtm extends SimpleBankAccount {
-    public SimpleBankAccountWithAtm(AccountHolder holder, double balance) {
+
+    private final double fee;
+
+    public SimpleBankAccountWithAtm(AccountHolder holder, double balance, double fee) {
         super(holder, balance);
+        this.fee = fee < 0 ? 0 : fee;
     }
+
+    @Override
+    public void deposit(final int userID, final double amount) {
+        if (checkUser(userID) && isDepositAllowed(amount)) {
+            this.balance += (amount - this.fee);
+        }
+    }
+
+    @Override
+    public void withdraw(final int userID, final double amount) {
+        super.withdraw(userID, amount + this.fee);
+    }
+
+    private boolean isWithdrawAllowed(final double amount) {
+        return this.balance >= amount + this.fee;
+    }
+
+    private boolean isDepositAllowed(final double amount) {
+        return amount > this.fee;
+    }
+
 }
