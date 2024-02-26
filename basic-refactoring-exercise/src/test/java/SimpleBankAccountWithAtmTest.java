58f018a0ab494d4517e90a6fe2a90a315b4ce6ff
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-02-26 10:43:03 +0100

    Create test SimpleBankAccountWithAtmTest

---
+++ basic-refactoring-exercise/src/test/java/SimpleBankAccountWithAtmTest.java
@@ -0,0 +1,13 @@
+import example.model.AccountHolder;
+import example.model.BankAccount;
+import example.model.SimpleBankAccount;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * The test suite for testing the SimpleBankAccountWithAtm implementation
+ */
+public class SimpleBankAccountWithAtmTest {
+
+}
