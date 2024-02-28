e919d67c4e92afa5bb577855902678bb60b2e44a
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-02-28 10:48:05 +0100

    Create CircularListWithFilteredNextTest

---
+++ tdd-exercise/src/test/java/tddfilter/CircularListWithFilteredNextTest.java
@@ -0,0 +1,12 @@
+package tddfilter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * The test suite for testing the CircularListWithFilteredNext implementation
+ */
+public class CircularListWithFilteredNextTest {
+
+}
